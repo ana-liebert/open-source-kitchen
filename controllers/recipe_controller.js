7e88b8cc3452f7c845f0ebb98bd3623c06d29ca8
najeesimmons <najee.bryant.simmons@gmail.com>
2022-01-25 13:01:28 -0500

    added two routes to controller

--- controllers/recipe_controller.js
+++ controllers/recipe_controller.js
@@ -1,3 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models')
+
+/*
+/home
+homes.ejs
+/recipe/id
+recipe.ejs
+post
+post.ejs
+category/breakdast || lunch || dinner
+category.ejs
+*/
+
+
+
+router.get("/new", function(req, res) {
+    res.render("post.ejs")
+})
+// this route will catch route to a form
+
+router.get('/:recipeId', async (req, res, next) => {
+    try {
+        const foundRecipe = await db.Recipe.findById(req.params.recipeId)
+        console.log(foundRecipe);
+        const context = { recipe: foundRecipe }
+        res.render('recipe.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+});
+// this route will catch GET requests to /recipe/index/ and respond with a single product
