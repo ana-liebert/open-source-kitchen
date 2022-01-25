1a40f678d76f03e3067b9e91731ad2af53b369b3
Ana Liebert <analiebert19@gmail.com>
2022-01-25 11:59:53 -0500

    added server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const controllers = require('./controllers');
+const methodOverride = require('method-override');
+
+const PORT = 4000
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+// app.use('/products', controllers.product);
+
+app.get('/', function (req, res) {
+    res.send('This is working');
+})
+
+app.get("/*", (req, res) => {
+    const context = { error: req.error };
+    return res.status(404).render("404", context);
+});
+
+app.listen(PORT, function() {
+    console.log('Listening on PORT 4000');
+})
